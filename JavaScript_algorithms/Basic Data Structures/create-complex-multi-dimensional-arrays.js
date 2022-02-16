29d63ae2f70f7dc21a324c8ffbc818a3551f1428
unknown <alex.178.ko@gmail.com>
2022-02-16 22:42:59 +0800

    Progess on Basic Data Structures

---
+++ JavaScript_algorithms/Basic Data Structures/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,20 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['1stdeep'],
+  [
+    ['deep'],[1]
+  ],
+  [
+    [
+      ['deeper']
+    ]
+  ],
+  [
+    [
+      [
+        ['deepest']
+      ]
+    ]
+  ]
+  // Only change code above this line
+];
