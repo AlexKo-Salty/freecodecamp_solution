9048bd74ac243e58f211a8d80434e2268d384ab0
unknown <alex.178.ko@gmail.com>
2022-04-25 21:04:00 +0800

    Completed styling of javascript calculator

--- Front_End_Development_Libraries/Front End Development Libraries Projects/build_a_javascript_calculator/src/App.js
+++ Front_End_Development_Libraries/Front End Development Libraries Projects/build_a_javascript_calculator/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import buttons from './button.json';
+
+const isOperator = /[X/+‑]/,
+endsWithOperator = /[X+‑/]$/,
+endsWithNegativeSign = /\d[X/+‑]{1}‑$/;
 
 {/* Button 
  Three prop will be passed to button:
@@ -14,7 +19,7 @@ class Button extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(e) {
-    //
+    this.props.action(e.target.outerText);
   }
   render() {
     return (
@@ -28,31 +33,83 @@ class Button extends React.Component {
 class Result extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      result: 0,
-      formula: ""
-    };
   }
   render() {
     return (
-      <div>
-        <p className='Display'>{this.state.formula}</p>
-        <p className='Result'>{this.state.result}</p>
+      <div id="result-section">
+        <div className='Result' id="result">{this.props.formula}</div>
+        <div className='Display' id="display">{this.props.result}</div>
       </div>
     )
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-        <div id="calculator">
-          <div id="result-section">
-            <Result />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      formula: ""
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+  buttonHandler(value) {
+    if (value === "AC") {
+      this.setState({
+        result: 0,
+        formula: ""
+      });
+    } else if (!Number.isNaN(parseInt(value))) {
+      this.setState((state) => {
+        const currentFormula = state.formula;
+        const currentResult = state.result;
+        return {
+          result: (currentResult === 0 || (currentResult === "+" || currentResult === "X" || currentResult === "/")) ? value : currentResult + value,
+          formula: currentFormula + value
+        };
+      });
+    } else if (value === "+" || value === "X" || value === "/" || value === "-" ) {
+      this.setState((state) => {
+        const currentFormula = state.formula;
+        const currentResult = state.result;
+        return {
+          result: value,
+          formula: currentFormula + value
+        };
+      });
+    } else if (value === "-" ) {
+
+    } else if (value === ".") {
+
+    } else if (value === "=") {
+      if (!this.state.formula.includes("="))
+      {
+        this.setState((state) => {
+          const currentFormula = state.formula;
+          const currentResult = state.result;
+          let replaceFormula = currentFormula.replace(/X/g, "*");
+          let calResult = eval(replaceFormula);
+          console.log(replaceFormula,calResult)
+          return {
+            result: calResult,
+            formula: currentFormula + "=" + calResult
+          };
+        });
+      }
+    }
+  }
+  render () {
+    return (
+      <div className="App">
+          <div id="calculator">
+              <Result formula={this.state.formula} result={this.state.result}/>
+            <div id="pad-section">
+              {buttons.map(s => <Button buttonStyle={s.buttonStyle} buttonDisplayValue={s.buttonDisplayValue} buttonID={s.buttonID} action={this.buttonHandler} />)}
+            </div>
           </div>
-        </div>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
